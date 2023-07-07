9283521bfaea731d96b7ba94013005dec7313d75
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-07 18:10:32 +0200

    add soft delete js confirmation

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,19 @@ import "~resources/scss/app.scss";
 import.meta.glob(["../img/**"]);
 
 import * as bootstrap from "bootstrap";
+
+
+const confirmDelete = document.querySelector("#btn-confirm-delete");
+
+if (confirmDelete) {
+    document.querySelectorAll(".js-delete").forEach((button) => {
+        // seleziono tutti i button con la classe js-delete
+        button.addEventListener("click", function () {
+            console.log("hai cliccato il bottone con id " + this.dataset.id);
+            confirmDelete.action = confirmDelete.dataset.template.replace(
+                "***",
+                this.dataset.id
+            );
+        });
+    });
+}
